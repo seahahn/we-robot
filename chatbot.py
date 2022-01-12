a650b69654870b29b6104152d46db74f8f05f066
seahahn <seah.ahn.nt@gmail.com>
2022-01-12 14:56:26 +0900

    first commit

---
+++ chatbot.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import argparse, torch
+from pytorch_lightning.core.lightning import LightningModule
+from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
+
+parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
+
+parser.add_argument('--chat',
+                    action='store_true',
+                    default=True,
+                    help='response generation on given user input')
+
+parser.add_argument('--sentiment',
+                    type=str,
+                    default='0',
+                    help='sentiment for system. 0 is neutral, 1 is negative, 2 is positive.')
+
+parser.add_argument('--model_params',
+                    type=str,
+                    default='model_chp/model_best.ckpt',
+                    help='model binary for starting chat')
+
+parser.add_argument('--train',
+                    action='store_true',
+                    default=False,
+                    help='for training')
+
+U_TKN = '<usr>'
+S_TKN = '<sys>'
+BOS = '</s>'
+EOS = '</s>'
+MASK = '<unused0>'
+SENT = '<unused1>'
+PAD = '<pad>'
+
+TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
+            bos_token=BOS, eos_token=EOS, unk_token='<unk>',
+            pad_token=PAD, mask_token=MASK) 
+
+class KoGPT2Chat(LightningModule):
+    def __init__(self, hparams, **kwargs):
+        super(KoGPT2Chat, self).__init__()
+        self.hparams = hparams
+        self.neg = -1e18
+        self.kogpt2 = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
+        self.loss_function = torch.nn.CrossEntropyLoss(reduction='none')
+        
+    def forward(self, inputs):
+        # (batch, seq_len, hiddens)
+        output = self.kogpt2(inputs, return_dict=True)
+        return output.logits
+
+    def chat(self, input, sent='0'):
+        tok = TOKENIZER
+        # sent_tokens = tok.tokenize(sent)
+        with torch.no_grad():
+            while 1:
+                q = input
+                # if q == 'quit':
+                #     break
+                a = ''
+                while 1:
+                    input_ids = torch.LongTensor(tok.encode(U_TKN + q + SENT + sent + S_TKN + a)).unsqueeze(dim=0)
+                    pred = self(input_ids)
+                    gen = tok.convert_ids_to_tokens(
+                        torch.argmax(
+                            pred,
+                            dim=-1).squeeze().numpy().tolist())[-1]
+                    if gen == EOS:
+                        break
+                    a += gen.replace('▁', ' ')
+                return a.strip()
+
+
+args = parser.parse_args()
+model = KoGPT2Chat.load_from_checkpoint(args.model_params)
+# model.chat()
---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room, leave_room
+from chatbot import model
+from langdetect import detect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cd_s4p_chatbot!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 사용자가 보낸 메시지를 받아서 챗봇에게 전달
+# 이와 함께 사용자의 채팅창에 사용자가 보낸 메시지 출력하기
+@socketio.on('send')
+def make_conversation(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    msg = json['message']
+    detected = detect(msg)
+    room = request.sid
+    if detected == 'ko':
+        socketio.emit('response', json, to=room) # 사용자 메시지 출력
+        
+        chatbot_resp = model.chat(msg) # 챗봇 메시지 요청하기
+        # chatbot_resp = "챗봇의 메시지가 도착했다!"
+        json = {
+            'user_name': 'We-robot',
+            'message': chatbot_resp
+            }
+        socketio.emit('response', json, to=room) # 챗봇 메시지 출력
+    else:
+        socketio.emit('response', json, to=room) # 사용자 메시지 출력
+        json = {
+            'user_name': '안내 : ',
+            'message': '챗봇과 대화하시려면 한글로 입력해주세요.'
+            }
+        socketio.emit('response', json, to=room)
+
+# 사용자 입장 시 방 생성하기
+@socketio.on('join')
+def on_join(data):
+    room = request.sid
+    join_room(room)
+    socketio.emit('user_state', data, to=room)
+    
+    json = {
+            'user_name': 'We-robot',
+            'message': '안녕하세요! 저는 당신을 위한 We-robot 입니다.'
+            }
+    socketio.emit('response', json, to=room) # 사용자 입장 시 챗봇의 환영 메시지 출력
+    print('user in: ' + str(room))
+
+# 사용자 연결 종료 시 방 없애기
+@socketio.on('disconnect')
+def test_disconnect():
+    room = request.sid
+    leave_room(room)
+    print('user out: ' + str(room))
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
